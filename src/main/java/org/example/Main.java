c16d969ea03286466f171ea071327c7b70f769e6
bladerunner-f <grant04x@mail.ru>
2023-12-17 13:50:50 +0300

    Перевод программы на английский.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,50 @@
 package org.example;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
+/**
+ * The Main class for querying the Star Wars API.
+ */
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+    //private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
-        logger.info("Starting program...");
+        //logger.info("Starting program...");
 
         boolean a = true;
 
         Map<String, String> reqRes = new HashMap<String, String>();
         reqRes.put("Luke Skywalker", "1");
         reqRes.put("Luke", "1");
-        reqRes.put("Люк", "1");
+        //reqRes.put("Люк", "1");
         reqRes.put("Darth Vader", "4");
-        reqRes.put("Дарт Вейдер", "4");
+        //reqRes.put("Дарт Вейдер", "4");
         reqRes.put("R2-D2", "3");
-        reqRes.put("Урна на колёсиках", "3");
-        reqRes.put("Оби-Ван Кеноби", "10");
-        reqRes.put("Оби-Ван", "10");
+        //reqRes.put("Урна на колёсиках", "3");
+        //reqRes.put("Оби-Ван Кеноби", "10");
+        //reqRes.put("Оби-Ван", "10");
         reqRes.put("Obi-Wan Kenobi", "10");
         reqRes.put("Dagobah", "5");
-        reqRes.put("дагоба", "5");
-        reqRes.put("Дагоба", "5");
+        //reqRes.put("дагоба", "5");
+        //reqRes.put("Дагоба", "5");
         reqRes.put("Bespin", "6");
-        reqRes.put("Беспин", "6");
-        reqRes.put("беспин", "6");
-        reqRes.put("Звезда смерти", "9");
+        //reqRes.put("Беспин", "6");
+        //reqRes.put("беспин", "6");
+        //reqRes.put("Звезда смерти", "9");
         reqRes.put("Death Star", "9");
 
         do {
-            System.out.println("\n\n          Консоль запросов к сервису Star Wars API");
+            System.out.println("\n\n          Star Wars API Query Console");
             System.out.println("-------------------------------------------------------------------");
-            System.out.println("1 - Вывести список доступных категорий");
-            System.out.println("2 - Вывести список доступных запросов");
-            System.out.println("3 - Выполнить запрос");
-            System.out.println("4 - Выход");
+            System.out.println("1 - Show available categories");
+            System.out.println("2 - Show available queries");
+            System.out.println("3 - Perform a query");
+            System.out.println("4 - Exit");
             try {
                 Scanner name = new Scanner(System.in);
                 System.out.print(": ");
@@ -51,42 +53,42 @@ public class Main {
 
                 switch (input){
                     case "1":
-                        System.out.println("Список доступных категорий : " + "people, planets, starships");
+                        System.out.println("Available categories: " + "people, planets, starships");
                         break;
                     case "2":
-                        System.out.println("Список доступных запросов: " + reqRes.keySet());
+                        System.out.println("Available queries: " + reqRes.keySet());
                         break;
                     case "3":
 
                         String category = StarWarsInfoApp.inputCategory();
                         String i1 = StarWarsInfoApp.inputName();
                         if (!reqRes.containsKey(i1)) {
-                            System.out.print("Неверный ввод");
+                            System.out.print("Invalid input");
                             break;
                         }
                         switch (category) {
                             case "planets":
                                 StarWarsPlanetDTO planet = (StarWarsPlanetDTO) StarWarsInfoApp.Info(category, reqRes.get(i1));
-                                System.out.println("Название: " + planet.getName());
-                                System.out.println("Численность населения: " + planet.getPopulation());
-                                System.out.println("Гравитация (g): " + planet.getGravity());
-                                System.out.println("Местность: " + planet.getTerrain());
-                                System.out.println("Диаметр: " + planet.getDiameter());
+                                System.out.println("Name: " + planet.getName());
+                                System.out.println("Population: " + planet.getPopulation());
+                                System.out.println("Gravity (g): " + planet.getGravity());
+                                System.out.println("Terrain: " + planet.getTerrain());
+                                System.out.println("Diameter: " + planet.getDiameter());
                                 break;
                             case "people":
                                 StarWarsCharacterDTO hero = (StarWarsCharacterDTO) StarWarsInfoApp.Info(category, reqRes.get(i1));
-                                System.out.println("Имя: " + hero.getName());
-                                System.out.println("Рост: " + hero.getHeight());
-                                System.out.println("Вес: " + hero.getMass());
-                                System.out.println("Пол: " + hero.getGender());
-                                System.out.println("Год рождения: " + hero.getBirth_year());
+                                System.out.println("Name: " + hero.getName());
+                                System.out.println("Height: " + hero.getHeight());
+                                System.out.println("Mass: " + hero.getMass());
+                                System.out.println("Gender: " + hero.getGender());
+                                System.out.println("Birth year: " + hero.getBirth_year());
                                 break;
                             case "starships":
                                 StarWarsStarshipsDTO starship = (StarWarsStarshipsDTO) StarWarsInfoApp.Info(category, reqRes.get(i1));
-                                System.out.println("Модель: "+ starship.getModel());
-                                System.out.println("Производитель: "+ starship.getManufacturer());
-                                System.out.println("Класс корабля: " + starship.getStarship_class());
-                                System.out.println("Кол-во экипажа: " + starship.getCrew());
+                                System.out.println("Model: "+ starship.getModel());
+                                System.out.println("Manufacturer: "+ starship.getManufacturer());
+                                System.out.println("Starship class: " + starship.getStarship_class());
+                                System.out.println("Crew: " + starship.getCrew());
                                 break;
                         }
                         break;
--- src/main/java/org/example/StarWarsInfoApp.java
+++ src/main/java/org/example/StarWarsInfoApp.java
@@ -7,9 +7,15 @@ import io.restassured.RestAssured;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Класс для получения информации о персонажах, планетах и звездолетах из звездных войн.
+ */
 public class StarWarsInfoApp {
 
-
+        /**
+         * Метод для ввода категории (персонаж, планета, звездолет)
+         * @return введенную категорию
+         */
         public static String inputCategory() {
             Scanner category = new Scanner(System.in);
             System.out.print("Введите название категории: ");
@@ -17,6 +23,10 @@ public class StarWarsInfoApp {
             return cat;
         }
 
+    /**
+     * Метод для ввода запроса (название персонажа, планеты, звездолета)
+     * @return введенный запрос
+     */
         public static String inputName(){
             Scanner name = new Scanner(System.in);
             System.out.print("Введите запрос: ");
@@ -24,12 +34,11 @@ public class StarWarsInfoApp {
             return id;
         }
 
-
     /**
-     * Проверяет что - то
-     * @param category
-     * @param name
-     * @return
+     * Метод для получения информации о категории и запросе
+     * @param category категория (персонаж, планета, звездолет)
+     * @param name запрос (название персонажа, планеты, звездолета)
+     * @return информация о категории и запросе в виде объекта DTO
      */
         public static DTO Info(String category, String name){
             String url = "https://swapi.dev/api/" + category + "/" +  name + "/";
@@ -45,6 +54,11 @@ public class StarWarsInfoApp {
         }
 
 
+    /**
+     * Метод для получения информации о персонаже из API
+     * @param url URL для получения информации
+     * @return информация о персонаже в виде объекта StarWarsCharacterDTO
+     */
         public static StarWarsCharacterDTO SWPeople (String url){
             Response response = RestAssured.get(url);
             String jsonResponse = response.body().asString();
@@ -56,8 +70,13 @@ public class StarWarsInfoApp {
                 throw new RuntimeException(e);
             }
             return characterDTO;
-
         }
+
+    /**
+     * Метод для получения информации о планете из API
+     * @param url URL для получения информации
+     * @return информация о планете в виде объекта StarWarsPlanetDTO
+     */
         public static StarWarsPlanetDTO SWPlanet (String url){
             Response response = RestAssured.get(url);
             String jsonResponse = response.body().asString();
@@ -70,7 +89,11 @@ public class StarWarsInfoApp {
             }
             return planetDTO;
         }
-
+    /**
+     * Метод для получения информации о звездолете из API
+     * @param url URL для получения информации
+     * @return информация о звездолете в виде объекта StarWarsStarshipsDTO
+     */
         public static StarWarsStarshipsDTO SWStarship (String url){
             Response response = RestAssured.get(url);
             String jsonResponse = response.body().asString();
