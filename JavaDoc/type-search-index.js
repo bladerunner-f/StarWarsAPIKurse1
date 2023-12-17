ffa32b596e2b2a4916f711bf4be9a8c15b22733b
Fedor Sit <fyodorsit@yandex.ru>
2023-12-17 13:58:20 +0300

    Добавил папку с JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.example","l":"DTO"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"StarWarsCharacterDTO"},{"p":"org.example","l":"StarWarsInfoApp"},{"p":"org.example","l":"StarWarsPlanetDTO"},{"p":"org.example","l":"StarWarsStarshipsDTO"}];updateSearchResults();
